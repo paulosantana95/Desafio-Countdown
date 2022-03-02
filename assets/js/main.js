0ceb18d69aede798986d943dca41951e42c950e6
Paulo Santana <paulosantana.sa@gmail.com>
2022-03-02 02:56:08 -0300

    temporary conclusion javascript timer countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,75 @@
-function timerStart(){
-    timer = setInterval(function(){
-        segundos--;
-        relogio.innerHTML = getTimeFromSeconds(segundos);
-    }, 1000);    
+'user strict';
+
+const secondsInMilliseconds = 1000;
+const minutesInMilliseconds = secondsInMilliseconds * 60;
+const hourInMilliseconds = minutesInMilliseconds * 60;
+const dayInMilliseconds = hourInMilliseconds * 24;
+
+const currentDate = new Date().getTime();
+const lastDate = new Date('2022-03-03 23:59:59').getTime();
+
+
+// function formatTime(time) {
+//     return time < 10 ? `0${time}` : time;
+// }
+
+
+// function countdown(){
+    
+//         const dateDiff = lastDate - currentDate;
+//         //days
+//         const days = Math.floor(dateDiff /dayInMilliseconds);
+//         //hours
+//         const hours = Math.floor(dateDiff % dayInMilliseconds / hourInMilliseconds);
+//         //minutes
+//         const minutes = Math.floor(dateDiff % hourInMilliseconds / minutesInMilliseconds);
+//         //seconds
+//         const seconds = Math.floor(dateDiff % minutesInMilliseconds / secondsInMilliseconds);
+
+//         const newDays = formatTime(days);
+//         const newHours = formatTime(hours);
+//         const newMinutes = formatTime(minutes);
+//         const newSeconds = formatTime(seconds);
+
+//         document.querySelector("h2").innerHTML = `${newDays} : ${newHours} : ${newMinutes} : ${newSeconds}`;
+// }
+// setTimeout(countdown, 1000);
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
 }
 
+const currentTime = (time) => {
+    const qtdSegundos = time % 60;
+    const qtdMinutos = Math.floor((time % (60 * 60)) / 60);
+    const qtdHoras = Math.floor((time % (60 * 60 * 24)) / (60 * 60));
+    const qtdDias = Math.floor(time / (60 * 60 * 24));
 
-//aqui vai sempre ser a hora atual
-var startDate = new Date();
-//como exemplo vou definir a data de fim com base na data atual
-var endDate = new Date();
-endDate.setDate(endDate.getDate()+5);
-
-//aqui é a diferenca entre as datas, basicamente é com isso que voce calcula o tempo restante
-var dateDiff;
-var days, hours, minutes, seconds;
-var $day = $('#dias');
-var $hour = $('#horas');
-var $minute = $('#minutos');
-var $second = $('#segundos');
-var $debug = $('#debug');
-var timer;
-
-function update(){
-	dateDiff = endDate - startDate;
-  dateDiff = dateDiff / 1000;
-  
-	seconds = Math.floor((dateDiff % 60));
-  
-  dateDiff = dateDiff / 60;
-  minutes = Math.floor((dateDiff % 60));
-	
-  dateDiff = dateDiff / 60;
-  hours = Math.floor((dateDiff%24));
-  
-	days = Math.floor(dateDiff/24);
-  
-  $day.text(days);
-  $hour.text(hours);
-  $minute.text(minutes);
-  $second.text(seconds);
-  
-  startDate.setSeconds(startDate.getSeconds()+1);
+    document.querySelector("h2").innerHTML = `${formatTime(qtdDias)}  :  ${formatTime(qtdHoras)}  :  ${formatTime(qtdMinutos)}  :  ${formatTime(qtdSegundos)}`
 }
-update();
-timer = setInterval(update, 1000);
+
+const countdown = (time) => {
+    const stopCount = () => clearInterval(timer)
+    const count = () => {
+        if(time == 0){
+            stopCount();
+        }
+        currentTime(time);
+        time--;
+
+    }
+    const timer = setInterval(count ,1000);
+}
+
+const timeLeft = () => {
+    return Math.floor((lastDate - currentDate)/1000);
+}
+
+countdown(timeLeft());
+
+
+
+
+
+
 
